587e22ff5d553d5507ccfc0185d52dab91d91c03
NicolasGabM <nicomoly525@gmail.com>
2022-10-27 00:58:17 -0300

    Carpetas de imagenes, js y estilos

---
+++ JS/header_footer.js
@@ -0,0 +1,14 @@
+document.getElementById("header").innerHTML=`<nav>
+<p><a href="index.html">PEGASUS</a></p>
+<ul>
+    <li><a class="headerA" href="variedades.html">Variedades</a></li>
+    <li><a class="headerA" href="historia.html">Historia</a></li>
+    <li><a class="headerA" href="contacto.html">Contacto</a> </li>
+</ul>
+</nav>`
+document.getElementById("footer").innerHTML=`
+    <p>info</p>
+    <p>info</p>
+    <p>info</p>
+    <p>agregar info sobre equipo y etc</p>
+`
