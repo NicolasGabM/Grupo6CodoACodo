c3e141e2bbeefd0ad9f399d40418d44c6c76f516
NicolasGabM <nicomoly525@gmail.com>
2022-10-27 01:45:23 -0300

    cambios

--- JS/header_footer.js
+++ JS/header_footer.js
@@ -2,13 +2,10 @@ document.getElementById("header").innerHTML=`<nav>
 <p><a href="index.html">PEGASUS</a></p>
 <ul>
     <li><a class="headerA" href="variedades.html">Variedades</a></li>
-    <li><a class="headerA" href="historia.html">Historia</a></li>
+    <li><a class="headerA" href="historia.html">Sobre nosotros</a></li>
     <li><a class="headerA" href="contacto.html">Contacto</a> </li>
 </ul>
 </nav>`
 document.getElementById("footer").innerHTML=`
-    <p>info</p>
-    <p>info</p>
-    <p>info</p>
-    <p>agregar info sobre equipo y etc</p>
+    <p>Equipo 6 codo a codo</p>
 `
