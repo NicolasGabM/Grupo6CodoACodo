9fc56c1654894536d894854a3317bf118abf8fc5
willian-alv <44449425+willian-alv@users.noreply.github.com>
2022-10-30 22:30:54 -0300

    buckup de main

--- JS/header_footer.js
+++ JS/header_footer.js
@@ -1,7 +1,7 @@
 document.getElementById("header").innerHTML=`<nav>
 <p class="text-shadow-pop-left"><a href="index.html">PEGASUS</a></p>
 <ul>
-    <li><a class="headerA" href="variedades.html">Variedades</a></li>
+    <li><a class="headerA" href="ingredientes.html">Ingredientes</a> </li>
     <li><a class="headerA" href="historia.html">Sobre nosotros</a></li>
     <li><a class="headerA" href="contacto.html">Contacto</a> </li>
 </ul>
